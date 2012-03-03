d2ab5a07f28eb44b79ae63c458ca2729b7b60d68
Bill Parrott <bill@chimericdream.com>
2012-03-03 11:37:09 -0600

    adding collapse arch plugin

--- base_framework_theme/wtf/admin/plugins.php
+++ base_framework_theme/wtf/admin/plugins.php
@@ -3,6 +3,9 @@
 if (!get_option('wtf-plugins-wp-calendar')) {
     add_option('wtf-plugins-wp-calendar', false);
 }
+if (!get_option('wtf-plugins-collapse-arch')) {
+    add_option('wtf-plugins-collapse-arch', false);
+}
 if (!get_option('wtf-plugins-google-calendar')) {
     add_option('wtf-plugins-google-calendar', false);
 }
@@ -16,9 +19,11 @@ function wtf_plugin_page() {
         $wp_cal = ($_POST['wp_calendar'] == 'true') ? true : false;
         $gcal = ($_POST['gcal'] == 'true') ? true : false;
         $twitter = ($_POST['twitter'] == 'true') ? true : false;
+        $collapse_arch = ($_POST['collapse_arch'] == 'true') ? true : false;
         update_option('wtf-plugins-wp-calendar', $wp_cal);
         update_option('wtf-plugins-google-calendar', $gcal);
         update_option('wtf-plugins-twitter', $twitter);
+        update_option('wtf-plugins-collapse-arch', $collapse_arch);
 
         $saved = true;
     }
@@ -38,59 +43,41 @@ function wtf_plugin_page() {
                 </div>
 
                 <!-- START OPTIONS -->
-                <div class="option">
-                    <label for="twitter_yes">Twitter Feed</label>
-                    <span class="description">This enables a plugin to show a Twitter feed in your sidebar.</span>
-                    <fieldset>
-                        <label for="twitter_yes"><input type="radio" value="true" id="twitter_yes" name="twitter"<?php
-    if ((bool) get_option('wtf-plugins-twitter') == true) {
-        echo ' checked="checked"';
-    }
-    ?> /> Yes</label><br />
-                        <label for="twitter_no"><input type="radio" value="false" id="twitter_no" name="twitter"<?php
-    if ((bool) get_option('wtf-plugins-twitter') == false) {
-        echo ' checked="checked"';
-    }
-    ?> /> No</label>
-                    </fieldset>
-                    <div class="clear"></div>
-                </div>
-
-                <div class="option">
-                    <label for="gcal_yes">Google Calendar</label>
-                    <span class="description">This enables you to use a Google Calendar feed for embedding in your site and sidebar.</span>
-                    <fieldset>
-                        <label for="gcal_yes"><input type="radio" value="true" id="gcal_yes" name="gcal"<?php
-    if ((bool) get_option('wtf-plugins-google-calendar') == true) {
-        echo ' checked="checked"';
-    }
-    ?> /> Yes</label><br />
-                        <label for="gcal_no"><input type="radio" value="false" id="gcal_no" name="gcal"<?php
-    if ((bool) get_option('wtf-plugins-google-calendar') == false) {
-        echo ' checked="checked"';
-    }
-    ?> /> No</label>
-                    </fieldset>
-                    <div class="clear"></div>
-                </div>
-
-                <div class="option">
-                    <label for="wp_calendar_yes">WordPress Calendar</label>
-                    <span class="description">This enables you to use a WordPress-based calendar for embedding in your site and sidebar.</span>
-                    <fieldset>
-                        <label for="wp_calendar_yes"><input type="radio" value="true" id="wp_calendar_yes" name="wp_calendar"<?php
-    if ((bool) get_option('wtf-plugins-wp-calendar') == true) {
-        echo ' checked="checked"';
-    }
-    ?> /> Yes</label><br />
-                        <label for="wp_calendar_no"><input type="radio" value="false" id="wp_calendar_no" name="wp_calendar"<?php
-    if ((bool) get_option('wtf-plugins-wp-calendar') == false) {
-        echo ' checked="checked"';
-    }
-    ?> /> No</label>
-                    </fieldset>
-                    <div class="clear"></div>
-                </div>
+                <?php
+                $plugins = array(
+                    array(
+                        'title'       => 'Calendar (Google)',
+                        'field_name'  => 'gcal',
+                        'note'        => 'This enables you to use a Google Calendar feed for embedding in your site and sidebar.',
+                        'option_name' => 'wtf-plugins-google-calendar',
+                    ),
+                    array(
+                        'title'       => 'Calendar (WordPress)',
+                        'field_name'  => 'wp_calendar',
+                        'note'        => 'This enables you to use a WordPress-based calendar for embedding in your site and sidebar.',
+                        'option_name' => 'wtf-plugins-wp-calendar',
+                    ),
+                    array(
+                        'title'       => 'Collapsing Archives',
+                        'field_name'  => 'collapse_arch',
+                        'note'        => 'This enables you to have either a widget or template function to enable collapsing archives in your sidebar. Useful if you want to display your archives but don\'t want to take up a lot of space with them.',
+                        'option_name' => 'wtf-plugins-collapse-arch',
+                    ),
+                    array(
+                        'title'       => 'Twitter Feed',
+                        'field_name'  => 'twitter',
+                        'note'        => 'This enables a plugin to show a Twitter feed in your sidebar.',
+                        'option_name' => 'wtf-plugins-twitter',
+                    ),
+                );
+                foreach ($plugins as $p) {
+                    $title       = $p['title'];
+                    $field_name  = $p['field_name'];
+                    $note        = $p['note'];
+                    $option_name = $p['option_name'];
+                    wtf_plugin_option_html($title, $field_name, $note, $option_name);
+                }
+                ?>
                 <!-- END OPTIONS -->
             </div>
             <!-- END SECTION -->
@@ -99,6 +86,29 @@ function wtf_plugin_page() {
     <div style="clear:both;height:20px;"></div>
     <?php
 } //end wtf_plugin_page
+
+function wtf_plugin_option_html($title, $field_name, $note, $option_name)
+{
+    ?>
+                <div class="option">
+                    <label for="<?php echo $field_name; ?>_yes"><?php echo $title; ?></label>
+                    <span class="description"><?php echo $note; ?></span>
+                    <fieldset>
+                        <label for="<?php echo $field_name; ?>_yes"><input type="radio" value="true" id="<?php echo $field_name; ?>_yes" name="<?php echo $field_name; ?>"<?php
+    if ((bool) get_option($option_name) == true) {
+        echo ' checked="checked"';
+    }
+    ?> /> Yes</label><br />
+                        <label for="<?php echo $field_name; ?>_no"><input type="radio" value="false" id="<?php echo $field_name; ?>_no" name="<?php echo $field_name; ?>"<?php
+    if ((bool) get_option($option_name) == false) {
+        echo ' checked="checked"';
+    }
+    ?> /> No</label>
+                    </fieldset>
+                    <div class="clear"></div>
+                </div>
+    <?php
+} //end wtf_plugin_option_html
 
 function wtf_plugin_main_page() {
     ?>
