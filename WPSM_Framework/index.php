38327d04196c766b88ad9e6c65fb581895c617a3
Bill Parrott <bill@chimericdream.com>
2012-08-12 20:40:41 -0500

    stripping framework "theme" to the bare minimum

--- WPSM_Framework/index.php
+++ WPSM_Framework/index.php
@@ -1,28 +1,81 @@
-<?php
-get_header();
-if (have_posts()) {
-    while (have_posts()) {
-        the_post();
-        ?>
-        <section <?php post_class(); ?> id="post-<?php the_ID(); ?>">
-            <header>
-                <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                <?php include (TEMPLATEPATH . '/inc/meta.php' ); ?>
-            </header>
-            <article class="entry">
-                <?php the_content(); ?>
-            </article>
-            <footer class="postmetadata">
-                <?php the_tags('Tags: ', ', ', '<br />'); ?>
-                Posted in <?php the_category(', '); ?> |
-                <?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?>
-            </footer>
-        </section>
-        <?php
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta http-equiv="Content-Type" content="<?php echo get_bloginfo('html_type'); ?>; charset=<?php echo get_bloginfo('charset'); ?>" />
+    <?php if (is_search()) : ?>
+        <meta name="robots" content="noindex, nofollow" />
+    <?php endif; ?>
+    <title><?php
+    if (function_exists('is_tag') && is_tag()) {
+        single_tag_title("Tag Archive for &quot;");
+        echo '&quot; - ';
+    } elseif (is_archive()) {
+        wp_title('');
+        echo ' Archive - ';
+    } elseif (is_search()) {
+        echo 'Search for &quot;' . wp_specialchars($s) . '&quot; - ';
+    } elseif (!(is_404()) && (is_single()) || (is_page() && !is_front_page())) {
+        wp_title('');
+        echo ' - ';
+    } elseif (is_404()) {
+        echo 'Not Found - ';
     }
-    include (TEMPLATEPATH . '/inc/nav.php' );
-} else {
-    echo '<h2>Not Found</h2>';
-}
-get_sidebar();
-get_footer();
+    if (is_home()) {
+        //Blog front page
+        bloginfo('name');
+        echo ' - ';
+        bloginfo('description');
+    } else {
+        bloginfo('name');
+    }
+    if ($paged > 1) {
+        echo ' - page '. $paged;
+    }
+    ?></title>
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+    <link rel="pingback" href="<?php echo get_bloginfo('pingback_url'); ?>" />
+
+    <?php if (is_singular()) : ?>
+        <?php wp_enqueue_script('comment-reply'); ?>
+    <?php endif; ?>
+    <?php wp_head(); ?>
+</head>
+    <body <?php body_class(); ?>>
+        <div class="header">
+            <h1><a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a></h1>
+            <div class="description"><?php bloginfo('description'); ?></div>
+        </div>
+        <div class="sidebar">
+        <?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('Sidebar Widgets')) : ?>
+            <!-- All this stuff in here only shows up if you DON'T have any widgets active in this zone -->
+            <?php get_search_form(); ?>
+            <?php wp_list_pages('title_li=<h2>Pages</h2>'); ?>
+            <h2>Archives</h2>';
+            <ul>';
+                <?php wp_get_archives('type=monthly'); ?>
+            </ul>
+            <h2>Categories</h2>';
+            <ul>
+                <?php wp_list_categories('show_count=1&title_li='); ?>
+            </ul>
+            <?php wp_list_bookmarks(); ?>
+            <h2>Meta</h2>
+            <ul>
+                <?php wp_register(); ?>
+                <li><?php echo wp_loginout('', false); ?></li>
+                <li><a href="http://wordpress.org/" title="Powered by WordPress, state-of-the-art semantic personal publishing platform.">WordPress</a></li>
+                <?php wp_meta(); ?>
+            </ul>
+            <h2>Subscribe</h2>
+            <ul>
+                <li><a href="<?php echo get_bloginfo('rss2_url'); ?>">Entries (RSS)</a></li>
+                <li><a href="<?php echo get_bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a></li>
+            </ul>
+        <?php endif; ?>
+        </div>
+        <div class="footer">
+            &copy;<?php echo date("Y"); echo " "; bloginfo('name'); ?>
+            <?php wp_footer(); ?>
+        </div>
+    </body>
+</html>
